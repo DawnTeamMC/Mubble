ce1011d9233a4cdddf41300e552e2be3028a7974
Hugman <contact.hugman@gmail.com>
2018-11-07 21:09:54 +0100

    Updated Annoying Dog

--- java/hugman/mod/objects/items/ItemAnnoyingDog.java
+++ java/hugman/mod/objects/items/ItemAnnoyingDog.java
@@ -1,12 +1,12 @@
 package hugman.mod.objects.items;
 
+import java.util.Random;
+
 import hugman.mod.init.ItemInit;
+import hugman.mod.util.handlers.SoundHandler;
 import hugman.mod.util.interfaces.IHasModel;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
@@ -16,7 +16,6 @@ import net.minecraft.world.World;
 
 public class ItemAnnoyingDog extends ItemBase implements IHasModel
 {
-	private PotionEffect[] effects;
 	public ItemAnnoyingDog()
 	{
 		super("annoying_dog");
@@ -24,21 +23,25 @@ public class ItemAnnoyingDog extends ItemBase implements IHasModel
 	}
 	
 	@Override
-	public boolean onDroppedByPlayer(ItemStack itemIn, EntityPlayer playerIn) {
-		World worldIn = playerIn.getEntityWorld();
-        worldIn.playSound((EntityPlayer)null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ENTITY_GHAST_SCREAM, SoundCategory.NEUTRAL, 0.5F, 1F);
-		return super.onDroppedByPlayer(itemIn, playerIn);
-	}
-	
-	@Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
     {
         ItemStack stack = playerIn.getHeldItem(handIn);
-        if (!playerIn.capabilities.isCreativeMode && stack.getCount() <= 63)
+        Random rand = new Random();
+        if(!worldIn.isRemote)
         {
-        	stack.grow(1);
+            int luck = rand.nextInt(20);
+            if (playerIn.capabilities.isCreativeMode) luck = 1;
+            if(luck == 0)
+            {
+            	stack.setCount(0);
+                worldIn.playSound((EntityPlayer)null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundHandler.ITEM_ANNOYING_DOG_DISAPPEAR, SoundCategory.PLAYERS, 0.5F, 1F);	
+            }
+            else
+            {
+            	if (!playerIn.capabilities.isCreativeMode && stack.getCount() <= 63) stack.grow(1);
+            	worldIn.playSound((EntityPlayer)null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundHandler.ITEM_ANNOYING_DOG_WAF, SoundCategory.PLAYERS, 0.5F, 1F);
+            }
             playerIn.addStat(StatList.getObjectUseStats(this));
-            worldIn.playSound((EntityPlayer)null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ENTITY_GHAST_SCREAM, SoundCategory.PLAYERS, 0.5F, 1F);
             return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
         }
         return new ActionResult<ItemStack>(EnumActionResult.FAIL, stack);
