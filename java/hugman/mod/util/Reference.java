3579ef155f24d7fb3f83a717b8a66304d19b7af3
Hugman <contact.hugman@gmail.com>
2019-01-18 18:54:15 +0100

    v1.3-pre5

--- java/hugman/mod/util/Reference.java
+++ java/hugman/mod/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MODID = "mubble";
 	public static final String NAME = "Mubble";
-	public static final String VERSION = "1.3-pre4";
+	public static final String VERSION = "1.3-pre5";
 	public static final String UPDATE_JSON = "https://raw.githubusercontent.com/Hugman76/Mubble/master/update.json";
 	
 	public static final String CLIENTPROXY = "hugman.mod.proxy.ClientProxy";
