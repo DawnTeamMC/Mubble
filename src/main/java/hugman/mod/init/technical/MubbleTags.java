f42fd37229cb7d92c8a03ecdb140c56b94991b06
Hugman <contact.hugman@gmail.com>
2019-03-23 12:55:01 +0100

    Tweaked Cloud Blocks

--- src/main/java/hugman/mod/init/technical/MubbleTags.java
+++ src/main/java/hugman/mod/init/technical/MubbleTags.java
@@ -1,16 +1,30 @@
 package hugman.mod.init.technical;
 
 import hugman.mod.Mubble;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
 import net.minecraft.util.ResourceLocation;
 
 public class MubbleTags
 {
+    public static class Blocks
+    {
+        public static final Tag<Block> CLOUD_BLOCKS = tag("cloud_blocks");
+
+        private static Tag<Block> tag(String name)
+        {
+            return new BlockTags.Wrapper(new ResourceLocation(Mubble.MOD_ID, name));
+        }
+    }
+    
     public static class Items
     {
         public static final Tag<Item> COINS = tag("coins");
+        public static final Tag<Item> CROWNS = tag("crowns");
+        public static final Tag<Item> LIGHTWEIGHT_ITEMS = tag("lightweight_items");
 
         private static Tag<Item> tag(String name)
         {
--- src/main/java/hugman/mod/objects/block/BlockCloud.java
+++ src/main/java/hugman/mod/objects/block/BlockCloud.java
@@ -4,17 +4,24 @@ import java.util.Random;
 
 import hugman.mod.Mubble;
 import hugman.mod.init.elements.MubbleBlocks;
+import hugman.mod.init.technical.MubbleTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 public class BlockCloud extends Block
 {
@@ -59,5 +66,28 @@ public class BlockCloud extends Block
     public int quantityDropped(IBlockState state, Random random)
     {
     	return 0;
+    }
+    
+    @Override
+    public void onEntityCollision(IBlockState state, World worldIn, BlockPos pos, Entity entityIn)
+    {
+    	if(entityIn instanceof EntityPlayer)
+    	{
+    		EntityPlayer playerIn = (EntityPlayer)entityIn;
+    		ItemStack armor;
+    		armor = playerIn.getItemStackFromSlot(EntityEquipmentSlot.HEAD);
+    		if(MubbleTags.Items.CROWNS.contains(armor.getItem()) && !playerIn.isSneaking())
+    		{
+        		if(!playerIn.isSprinting()) playerIn.motionY = (this.RANDOM.nextInt(31) + 40) / 100D;
+    			else playerIn.motionY = 0.7D;
+        		playerIn.fallDistance = 0f;
+    		}
+    	}
+    	if(entityIn instanceof EntityItem)
+    	{
+    		EntityItem itemIn = (EntityItem)entityIn;
+        	if(MubbleTags.Items.CROWNS.contains(itemIn.getItem().getItem())) itemIn.motionY = 0.3D;
+        	if(MubbleTags.Items.LIGHTWEIGHT_ITEMS.contains(itemIn.getItem().getItem())) itemIn.motionY = 0.1D;
+    	}
     }
 }
