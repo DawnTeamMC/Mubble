5ece43f484439b5d77a5500d5dd05e1497d8e9b8
Hugman <contact.hugman@gmail.com>
2019-05-05 13:45:55 +0200

    Small fixes + Added texture for heaviness effect

--- src/main/java/hugman/mod/init/MubblePotionEffects.java
+++ src/main/java/hugman/mod/init/MubblePotionEffects.java
@@ -3,14 +3,14 @@ package hugman.mod.init;
 import java.util.ArrayList;
 import java.util.List;
 
-import hugman.mod.objects.potion.effect.PotionHeaviness;
+import hugman.mod.objects.potion.effect.PotionSimple;
 import net.minecraft.potion.Potion;
 
 public class MubblePotionEffects
 {
     public static final List<Potion> EFFECTS = new ArrayList<Potion>();
 
-    public static final Potion HEAVINESS = new PotionHeaviness();
+    public static final Potion HEAVINESS = new PotionSimple("heaviness", true, 9198906);
     
     public static void register(Potion potion)
     {
---
+++ src/main/java/hugman/mod/objects/potion/effect/PotionSimple.java
@@ -0,0 +1,46 @@
+package hugman.mod.objects.potion.effect;
+
+import hugman.mod.Mubble;
+import hugman.mod.init.MubblePotionEffects;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class PotionSimple extends Potion
+{	
+	private final ResourceLocation iconTexture;
+	
+	public PotionSimple(String name, boolean isBadEffectIn, int liquidColorIn)
+	{
+		super(isBadEffectIn, liquidColorIn);
+		setRegistryName(Mubble.MOD_ID, name);
+		iconTexture = new ResourceLocation(Mubble.MOD_ID, "textures/gui/effects/" + name + ".png");
+		MubblePotionEffects.register(this);
+	}
+	
+	@Override
+	protected Potion setIconIndex(int x, int y)
+	{
+		return super.setIconIndex(0, 0);
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void renderInventoryEffect(PotionEffect effect, Gui gui, int x, int y, float z)
+	{
+		Minecraft.getInstance().getTextureManager().bindTexture(iconTexture);
+		Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0, 0, 18, 18, 18, 18);
+	}
+	
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void renderHUDEffect(PotionEffect effect, Gui gui, int x, int y, float z, float alpha)
+	{
+		Minecraft.getInstance().getTextureManager().bindTexture(iconTexture);
+		Gui.drawModalRectWithCustomSizedTexture(x + 3, y + 3, 0, 0, 18, 18, 18, 18);
+	}
+}
