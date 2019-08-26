b7c886ecdd6e0978f684b6547ff3bd8fe89a5c35
Hugman <contact.hugman@gmail.com>
2019-08-26 18:13:20 +0200

    Fixed #43

--- src/main/java/hugman/mubble/objects/item/SuperStarItem.java
+++ src/main/java/hugman/mubble/objects/item/SuperStarItem.java
@@ -2,8 +2,10 @@ package hugman.mubble.objects.item;
 
 import hugman.mubble.init.MubbleSounds;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 
 public class SuperStarItem extends Item
@@ -16,7 +18,7 @@ public class SuperStarItem extends Item
     @Override
     public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving)
     {
-    	entityLiving.playSound(MubbleSounds.ITEM_SUPER_STAR_THEME, 60000000, 1);
+    	worldIn.playMovingSound((PlayerEntity)null, entityLiving, MubbleSounds.ITEM_SUPER_STAR_THEME, SoundCategory.PLAYERS, 1.0F, 1.0F);
 		return super.onItemUseFinish(stack, worldIn, entityLiving);
     }
 }
