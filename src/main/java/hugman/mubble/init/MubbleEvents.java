fe861c6f7d1554b0936c9facdba4c0c226bf6297
Hugman <contact.hugman@gmail.com>
2019-08-29 12:45:43 +0200

    Added missing vanilla hand animations
    (i.e. throwing items, dyeing a pet's collar...)

--- src/main/java/hugman/mubble/init/MubbleEvents.java
+++ src/main/java/hugman/mubble/init/MubbleEvents.java
@@ -1,17 +1,37 @@
 package hugman.mubble.init;
 
 import hugman.mubble.init.world.MubbleDimensions;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.EndPortalFrameBlock;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.DyeItem;
+import net.minecraft.item.EggItem;
+import net.minecraft.item.EnderEyeItem;
+import net.minecraft.item.EnderPearlItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.LingeringPotionItem;
+import net.minecraft.item.SnowballItem;
+import net.minecraft.item.SplashPotionItem;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteract;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickItem;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -19,7 +39,7 @@ import net.minecraftforge.fml.common.Mod;
 public class MubbleEvents
 {
 	@SubscribeEvent
-	public static void jump(LivingJumpEvent event)
+	public static void onJump(LivingJumpEvent event)
 	{
 		LivingEntity entity = event.getEntityLiving();
 		if(entity.isPotionActive(MubbleEffects.HEAVINESS))
@@ -30,7 +50,7 @@ public class MubbleEvents
 	}
 	
 	@SubscribeEvent
-	public static void tick(LivingUpdateEvent event)
+	public static void onTick(LivingUpdateEvent event)
 	{
 		LivingEntity entity = event.getEntityLiving();
 		World world = entity.getEntityWorld();
@@ -49,6 +69,117 @@ public class MubbleEvents
 		if(!world.isRemote)
 		{
 			if(MubbleTags.Items.WEIGHT_HEAVY.contains(itemHead.getItem())) entity.addPotionEffect(new EffectInstance(MubbleEffects.HEAVINESS, 5, 0));
+		}
+	}
+	
+	@SubscribeEvent
+	public static void onItemToss(ItemTossEvent event)
+	{
+		PlayerEntity player = event.getPlayer();
+		
+		player.swingArm(Hand.MAIN_HAND);
+		player.playSound(SoundEvents.ENTITY_ITEM_PICKUP, 0.2F, 1.0F);
+	}
+	
+	@SubscribeEvent
+	public static void onItemRightClick(RightClickItem event)
+	{
+		PlayerEntity player = event.getPlayer();
+		Hand hand = event.getHand();
+		ItemStack itemStack = event.getItemStack();
+		
+		if(itemStack.getItem() instanceof EnderPearlItem)
+		{
+			player.swingArm(hand);
+		}
+		if(itemStack.getItem() instanceof SnowballItem)
+		{
+			player.swingArm(hand);
+		}
+		if(itemStack.getItem() instanceof SplashPotionItem)
+		{
+			player.swingArm(hand);
+		}
+		if(itemStack.getItem() instanceof LingeringPotionItem)
+		{
+			player.swingArm(hand);
+		}
+		if(itemStack.getItem() instanceof EggItem)
+		{
+			player.swingArm(hand);
+		}
+		if(itemStack.getItem() instanceof EnderEyeItem)
+		{
+			player.swingArm(hand);
+		}
+	}
+	
+	@SubscribeEvent
+	public static void onBlockRightClick(RightClickBlock event)
+	{
+		World worldIn = event.getWorld();
+		PlayerEntity player = event.getPlayer();
+		Hand hand = event.getHand();
+		BlockState blockState = worldIn.getBlockState(event.getPos());
+		ItemStack itemStack = event.getItemStack();
+		
+		if(itemStack.getItem() instanceof EnderEyeItem)
+		{
+			if(blockState.getBlock() == Blocks.END_PORTAL_FRAME)
+			{
+				if(!blockState.get(EndPortalFrameBlock.EYE))
+				{
+					player.swingArm(hand);
+				}
+			}
+			else
+			{
+				player.swingArm(hand);
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public static void onEntityRightClick(EntityInteract event)
+	{
+		PlayerEntity player = event.getPlayer();
+		Hand hand = event.getHand();
+		LivingEntity target = (LivingEntity)event.getTarget();
+		
+		ItemStack itemStack = event.getItemStack();
+		Item item = itemStack.getItem();
+		
+		if(item instanceof DyeItem && target instanceof SheepEntity)
+		{
+			SheepEntity sheep = (SheepEntity)target;
+			DyeItem dye = (DyeItem)item;
+			
+			if(sheep.isAlive() && !sheep.getSheared() && sheep.getFleeceColor() != dye.getDyeColor())
+			{
+				player.swingArm(hand);
+			}
+		}
+		
+		if(item instanceof DyeItem && target instanceof CatEntity)
+		{
+			CatEntity cat = (CatEntity)target;
+			DyeItem dye = (DyeItem)item;
+			
+			if(cat.isTamed() && cat.isOwner(player) && cat.getCollarColor() != dye.getDyeColor())
+			{
+				player.swingArm(hand);
+			}
+		}
+		
+		if(item instanceof DyeItem && target instanceof WolfEntity)
+		{
+			WolfEntity wolf = (WolfEntity)target;
+			DyeItem dye = (DyeItem)item;
+			
+			if(wolf.isTamed() && !itemStack.isEmpty() && wolf.getCollarColor() != dye.getDyeColor())
+			{
+				player.swingArm(hand);
+			}
 		}
 	}
 }
