29b60f3db7f9d8c2ab66c6abf87c6db9e14d098f
Hugman <contact.hugman@gmail.com>
2019-11-24 13:54:00 +0100

    Fixed #49

--- src/main/java/hugman/mubble/objects/block/TetrisGlassBlock.java
+++ src/main/java/hugman/mubble/objects/block/TetrisGlassBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraftforge.api.distmarker.Dist;
@@ -18,32 +19,44 @@ public class TetrisGlassBlock extends FallingBlock
     }
     
     @Override
+    @OnlyIn(Dist.CLIENT)
+    public boolean isSideInvisible(BlockState state, BlockState adjacentBlockState, Direction side)
+    {
+    	return adjacentBlockState.getBlock() == this ? true : false;
+    }
+    
+    @Override
 	public BlockRenderLayer getRenderLayer()
     {
     	return BlockRenderLayer.TRANSLUCENT;
 	}
     
+    @Override
     @OnlyIn(Dist.CLIENT)
-    public float func_220080_a(BlockState state, IBlockReader worldIn, BlockPos pos)
+    public float getAmbientOcclusionLightValue(BlockState state, IBlockReader worldIn, BlockPos pos)
     {
-       return 1.0F;
+    	return 1.0F;
     }
 
+    @Override
     public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos)
     {
        return true;
     }
 
+    @Override
     public boolean causesSuffocation(BlockState state, IBlockReader worldIn, BlockPos pos)
     {
        return false;
     }
 
+    @Override
     public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos)
     {
        return false;
     }
 
+    @Override
     public boolean canEntitySpawn(BlockState state, IBlockReader worldIn, BlockPos pos, EntityType<?> type)
     {
        return false;
