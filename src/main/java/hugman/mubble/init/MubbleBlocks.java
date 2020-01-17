4e2eb278e472171869b067af3c8d0f3d2e32575c
Hugman <contact.hugman@gmail.com>
2020-01-17 22:49:50 +0100

    Added Black, White, Purple and Golden Presents
    + Updated textures of Blue and Yellow Presents

--- src/main/java/hugman/mubble/init/MubbleBlocks.java
+++ src/main/java/hugman/mubble/init/MubbleBlocks.java
@@ -694,10 +694,14 @@ public class MubbleBlocks
     public static final Block SILVER_SHINY_GARLAND = register("silver_shiny_garland", new GarlandBlock(Properties.create(Material.LEAVES, MaterialColor.CLAY).hardnessAndResistance(0.2F).sound(SoundType.PLANT).lightValue(10)), ItemGroup.DECORATIONS, OTHERS, 30, 60);
     public static final Block GOLD_SHINY_GARLAND = register("gold_shiny_garland", new GarlandBlock(Properties.create(Material.LEAVES, MaterialColor.GOLD).hardnessAndResistance(0.2F).sound(SoundType.PLANT).lightValue(10)), ItemGroup.DECORATIONS, OTHERS, 30, 60);
 
+    public static final Block WHITE_PRESENT = register("white_present", new PresentBlock(Properties.create(Material.WOOD, MaterialColor.WHITE_TERRACOTTA).hardnessAndResistance(0.8F).sound(SoundType.WOOD)), ItemGroup.DECORATIONS, OTHERS, 60, 60);
+    public static final Block BLACK_PRESENT = register("black_present", new PresentBlock(Properties.create(Material.WOOD, MaterialColor.BLACK_TERRACOTTA).hardnessAndResistance(0.8F).sound(SoundType.WOOD)), ItemGroup.DECORATIONS, OTHERS, 60, 60);
     public static final Block BLUE_PRESENT = register("blue_present", new PresentBlock(Properties.create(Material.WOOD, MaterialColor.BLUE_TERRACOTTA).hardnessAndResistance(0.8F).sound(SoundType.WOOD)), ItemGroup.DECORATIONS, OTHERS, 60, 60);
     public static final Block GREEN_PRESENT = register("green_present", new PresentBlock(Properties.create(Material.WOOD, MaterialColor.GREEN_TERRACOTTA).hardnessAndResistance(0.8F).sound(SoundType.WOOD)), ItemGroup.DECORATIONS, OTHERS, 60, 60);
     public static final Block YELLOW_PRESENT = register("yellow_present", new PresentBlock(Properties.create(Material.WOOD, MaterialColor.YELLOW_TERRACOTTA).hardnessAndResistance(0.8F).sound(SoundType.WOOD)), ItemGroup.DECORATIONS, OTHERS, 60, 60);
     public static final Block RED_PRESENT = register("red_present", new PresentBlock(Properties.create(Material.WOOD, MaterialColor.RED_TERRACOTTA).hardnessAndResistance(0.8F).sound(SoundType.WOOD)), ItemGroup.DECORATIONS, OTHERS, 60, 60);
+    public static final Block PURPLE_PRESENT = register("purple_present", new PresentBlock(Properties.create(Material.WOOD, MaterialColor.PURPLE_TERRACOTTA).hardnessAndResistance(0.8F).sound(SoundType.WOOD)), ItemGroup.DECORATIONS, OTHERS, 60, 60);
+    public static final Block GOLDEN_PRESENT = register("golden_present", new PresentBlock(Properties.create(Material.WOOD, MaterialColor.GOLD).hardnessAndResistance(0.8F).sound(SoundType.WOOD)), ItemGroup.DECORATIONS, OTHERS, 60, 60);
     
     public static final Block FOOTBLOCK = register("footblock", new Block(Properties.from(Blocks.WHITE_WOOL)), ItemGroup.DECORATIONS, OTHERS, 60, 60);
     
--- src/main/java/hugman/mubble/init/data/MubbleTileEntityTypes.java
+++ src/main/java/hugman/mubble/init/data/MubbleTileEntityTypes.java
@@ -17,7 +17,7 @@ public class MubbleTileEntityTypes
     public static final List<TileEntityType<?>> TILE_ENTITY_TYPES = new ArrayList<TileEntityType<?>>();
 
     public static final TileEntityType<DispenserTileEntity> PLACER = type("placer", TileEntityType.Builder.create(PlacerTileEntity::new, MubbleBlocks.PLACER));
-    public static final TileEntityType<PresentTileEntity> PRESENT = type("present", TileEntityType.Builder.create(PresentTileEntity::new, MubbleBlocks.RED_PRESENT, MubbleBlocks.GREEN_PRESENT));
+    public static final TileEntityType<PresentTileEntity> PRESENT = type("present", TileEntityType.Builder.create(PresentTileEntity::new, MubbleBlocks.WHITE_PRESENT, MubbleBlocks.BLACK_PRESENT, MubbleBlocks.BLUE_PRESENT, MubbleBlocks.GREEN_PRESENT, MubbleBlocks.YELLOW_PRESENT, MubbleBlocks.RED_PRESENT, MubbleBlocks.PURPLE_PRESENT, MubbleBlocks.GOLDEN_PRESENT));
     
     private static <T extends TileEntity> TileEntityType<T> type(String name, TileEntityType.Builder<T> builder)
     {
