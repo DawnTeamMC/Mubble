13dd03073542fd55a62c3e5826b5a6132f5114f3
Yanis48 <doublecraft.official@gmail.com>
2020-01-26 13:04:09 +0100

    telekinesis enchantment events

--- src/main/java/hugman/mubble/mixin/LivingEntityMixin.java
+++ src/main/java/hugman/mubble/mixin/LivingEntityMixin.java
@@ -1,23 +1,31 @@
 package hugman.mubble.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import hugman.mubble.init.MubbleEffects;
+import hugman.mubble.init.MubbleEnchantments;
 import hugman.mubble.init.data.MubbleTags;
 import hugman.mubble.objects.item.LightsaberItem;
+import hugman.mubble.util.EnchantmentUtil;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.context.LootContext;
+import net.minecraft.loot.context.LootContextTypes;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin
+public abstract class LivingEntityMixin
 {
 	@Inject(method = "jump", at = @At(value = "TAIL"), cancellable = true)
 	private void jump(CallbackInfo ci)
@@ -55,4 +63,47 @@ public class LivingEntityMixin
 			((LightsaberItem) stack.getItem()).onSwing(entity, false);
 		}
 	}
+	
+	@Inject(method = "dropLoot", at = @At(value = "HEAD"), cancellable = true)
+	private void dropLoot(DamageSource source, boolean causedByPlayer, CallbackInfo ci)
+	{
+		LivingEntity entity = (LivingEntity) (Object) this;
+		World world = entity.getEntityWorld();
+		if(causedByPlayer)
+		{
+			PlayerEntity player = (PlayerEntity) source.getAttacker();
+			LootTable lootTable = world.getServer().getLootManager().getSupplier(entity.getLootTable());
+			LootContext.Builder builder = this.getLootContextBuilder(causedByPlayer, source);
+			if(EnchantmentUtil.hasEnchantment(MubbleEnchantments.TELEKINESIS, player.getMainHandStack()))
+			{
+				for(ItemStack stack : lootTable.getDrops(builder.build(LootContextTypes.ENTITY)))
+				{
+					player.inventory.insertStack(stack);
+				}
+				ci.cancel();
+			}
+		}
+	}
+	
+	@Inject(method = "dropXp", at = @At(value = "HEAD"), cancellable = true)
+	private void dropXp(CallbackInfo ci)
+	{
+		LivingEntity entity = (LivingEntity) (Object) this;
+		LivingEntity attacker = entity.getAttacker();
+		if(attacker instanceof PlayerEntity)
+		{
+			PlayerEntity player = (PlayerEntity) attacker;
+			if(!player.getMainHandStack().isEmpty())
+			{
+				if(EnchantmentUtil.hasEnchantment(MubbleEnchantments.TELEKINESIS, player.getMainHandStack()))
+				{
+					player.addExperience(this.getCurrentExperience(player));
+					ci.cancel();
+				}
+			}
+		}
+	}
+	
+	@Shadow protected abstract int getCurrentExperience(PlayerEntity player);
+	@Shadow protected abstract LootContext.Builder getLootContextBuilder(boolean causedByPlayer, DamageSource source);
 }
