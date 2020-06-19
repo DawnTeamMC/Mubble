ee8d564a5d0547f3ae74cef61fdd437c49d2a3b8
Hugman <contact.hugman@gmail.com>
2020-06-19 19:00:35 +0200

    Actually forgot half the files in the latest commit

--- src/main/java/hugman/mubble/init/MubbleBlocks.java
+++ src/main/java/hugman/mubble/init/MubbleBlocks.java
@@ -42,40 +42,45 @@ public class MubbleBlocks {
 	protected static final Block.Settings pStoneButton = FabricBlockSettings.of(Material.STONE).collidable(true).hardness(0.5F);
 
 	/* MUBBLE */
-	public static final Block OAK_VERTICAL_SLAB = register("oak_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.OAK_PLANKS)), ItemGroup.BUILDING_BLOCKS, 5, 20);
-	public static final Block SPRUCE_VERTICAL_SLAB = register("spruce_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SPRUCE_PLANKS)), ItemGroup.BUILDING_BLOCKS, 5, 20);
-	public static final Block BIRCH_VERTICAL_SLAB = register("birch_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.BIRCH_PLANKS)), ItemGroup.BUILDING_BLOCKS, 5, 20);
-	public static final Block JUNGLE_VERTICAL_SLAB = register("jungle_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.JUNGLE_PLANKS)), ItemGroup.BUILDING_BLOCKS, 5, 20);
-	public static final Block ACACIA_VERTICAL_SLAB = register("acacia_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.ACACIA_PLANKS)), ItemGroup.BUILDING_BLOCKS, 5, 20);
-	public static final Block DARK_OAK_VERTICAL_SLAB = register("dark_oak_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.DARK_OAK_PLANKS)), ItemGroup.BUILDING_BLOCKS, 5, 20);
-	public static final Block STONE_VERTICAL_SLAB = register("stone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.STONE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block SMOOTH_STONE_VERTICAL_SLAB = register("smooth_stone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SMOOTH_STONE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block COBBLESTONE_VERTICAL_SLAB = register("cobblestone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.COBBLESTONE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block MOSSY_COBBLESTONE_VERTICAL_SLAB = register("mossy_cobblestone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.MOSSY_COBBLESTONE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block STONE_BRICK_VERTICAL_SLAB = register("stone_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.STONE_BRICKS)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block MOSSY_STONE_BRICK_VERTICAL_SLAB = register("mossy_stone_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.MOSSY_STONE_BRICKS)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block GRANITE_VERTICAL_SLAB = register("granite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.GRANITE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block POLISHED_GRANITE_VERTICAL_SLAB = register("polished_granite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.POLISHED_GRANITE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block DIORITE_VERTICAL_SLAB = register("diorite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.DIORITE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block POLISHED_DIORITE_VERTICAL_SLAB = register("polished_diorite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.POLISHED_DIORITE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block ANDESITE_VERTICAL_SLAB = register("andesite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.ANDESITE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block POLISHED_ANDESITE_VERTICAL_SLAB = register("polished_andesite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.POLISHED_ANDESITE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block BRICK_VERTICAL_SLAB = register("brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.BRICKS)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block SANDSTONE_VERTICAL_SLAB = register("sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SANDSTONE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block CUT_SANDSTONE_VERTICAL_SLAB = register("cut_sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.CUT_SANDSTONE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block SMOOTH_SANDSTONE_VERTICAL_SLAB = register("smooth_sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SMOOTH_SANDSTONE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block RED_SANDSTONE_VERTICAL_SLAB = register("red_sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.RED_SANDSTONE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block CUT_RED_SANDSTONE_VERTICAL_SLAB = register("cut_red_sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.CUT_RED_SANDSTONE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block SMOOTH_RED_SANDSTONE_VERTICAL_SLAB = register("smooth_red_sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SMOOTH_RED_SANDSTONE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block NETHER_BRICK_VERTICAL_SLAB = register("nether_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.NETHER_BRICKS)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block RED_NETHER_BRICK_VERTICAL_SLAB = register("red_nether_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.RED_NETHER_BRICKS)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block QUARTZ_VERTICAL_SLAB = register("quartz_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.QUARTZ_BLOCK)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block SMOOTH_QUARTZ_VERTICAL_SLAB = register("smooth_quartz_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SMOOTH_QUARTZ)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block END_STONE_BRICK_VERTICAL_SLAB = register("end_stone_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.END_STONE_BRICKS)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block PURPUR_VERTICAL_SLAB = register("purpur_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.PURPUR_BLOCK)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block PRISMARINE_VERTICAL_SLAB = register("prismarine_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.PRISMARINE)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block PRISMARINE_BRICK_VERTICAL_SLAB = register("prismarine_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.PRISMARINE_BRICKS)), ItemGroup.BUILDING_BLOCKS);
-	public static final Block DARK_PRISMARINE_VERTICAL_SLAB = register("dark_prismarine_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.DARK_PRISMARINE)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block OAK_VERTICAL_SLAB = register("oak_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.OAK_SLAB)), ItemGroup.BUILDING_BLOCKS, 5, 20);
+	public static final Block SPRUCE_VERTICAL_SLAB = register("spruce_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SPRUCE_SLAB)), ItemGroup.BUILDING_BLOCKS, 5, 20);
+	public static final Block BIRCH_VERTICAL_SLAB = register("birch_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.BIRCH_SLAB)), ItemGroup.BUILDING_BLOCKS, 5, 20);
+	public static final Block JUNGLE_VERTICAL_SLAB = register("jungle_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.JUNGLE_SLAB)), ItemGroup.BUILDING_BLOCKS, 5, 20);
+	public static final Block ACACIA_VERTICAL_SLAB = register("acacia_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.ACACIA_SLAB)), ItemGroup.BUILDING_BLOCKS, 5, 20);
+	public static final Block DARK_OAK_VERTICAL_SLAB = register("dark_oak_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.DARK_OAK_SLAB)), ItemGroup.BUILDING_BLOCKS, 5, 20);
+	public static final Block CRIMSON_VERTICAL_SLAB = register("crimson_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.CRIMSON_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block WARPED_VERTICAL_SLAB = register("warped_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.WARPED_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block STONE_VERTICAL_SLAB = register("stone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.STONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block SMOOTH_STONE_VERTICAL_SLAB = register("smooth_stone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SMOOTH_STONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block COBBLESTONE_VERTICAL_SLAB = register("cobblestone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.COBBLESTONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block MOSSY_COBBLESTONE_VERTICAL_SLAB = register("mossy_cobblestone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.MOSSY_COBBLESTONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block STONE_BRICK_VERTICAL_SLAB = register("stone_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.STONE_BRICK_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block MOSSY_STONE_BRICK_VERTICAL_SLAB = register("mossy_stone_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.MOSSY_STONE_BRICK_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block GRANITE_VERTICAL_SLAB = register("granite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.GRANITE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block POLISHED_GRANITE_VERTICAL_SLAB = register("polished_granite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.POLISHED_GRANITE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block DIORITE_VERTICAL_SLAB = register("diorite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.DIORITE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block POLISHED_DIORITE_VERTICAL_SLAB = register("polished_diorite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.POLISHED_DIORITE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block ANDESITE_VERTICAL_SLAB = register("andesite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.ANDESITE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block POLISHED_ANDESITE_VERTICAL_SLAB = register("polished_andesite_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.POLISHED_ANDESITE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block BRICK_VERTICAL_SLAB = register("brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.BRICK_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block SANDSTONE_VERTICAL_SLAB = register("sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SANDSTONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block CUT_SANDSTONE_VERTICAL_SLAB = register("cut_sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.CUT_SANDSTONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block SMOOTH_SANDSTONE_VERTICAL_SLAB = register("smooth_sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SMOOTH_SANDSTONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block RED_SANDSTONE_VERTICAL_SLAB = register("red_sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.RED_SANDSTONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block CUT_RED_SANDSTONE_VERTICAL_SLAB = register("cut_red_sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.CUT_RED_SANDSTONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block SMOOTH_RED_SANDSTONE_VERTICAL_SLAB = register("smooth_red_sandstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SMOOTH_RED_SANDSTONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block NETHER_BRICK_VERTICAL_SLAB = register("nether_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.NETHER_BRICK_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block RED_NETHER_BRICK_VERTICAL_SLAB = register("red_nether_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.RED_NETHER_BRICK_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block QUARTZ_VERTICAL_SLAB = register("quartz_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.QUARTZ_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block SMOOTH_QUARTZ_VERTICAL_SLAB = register("smooth_quartz_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.SMOOTH_QUARTZ_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block END_STONE_BRICK_VERTICAL_SLAB = register("end_stone_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.END_STONE_BRICK_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block PURPUR_VERTICAL_SLAB = register("purpur_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.PURPUR_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block PRISMARINE_VERTICAL_SLAB = register("prismarine_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.PRISMARINE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block PRISMARINE_BRICK_VERTICAL_SLAB = register("prismarine_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.PRISMARINE_BRICK_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block DARK_PRISMARINE_VERTICAL_SLAB = register("dark_prismarine_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.DARK_PRISMARINE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block BLACKSTONE_VERTICAL_SLAB = register("blackstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.BLACKSTONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block POLISHED_BLACKSTONE_BRICK_VERTICAL_SLAB = register("polished_blackstone_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.POLISHED_BLACKSTONE_BRICK_SLAB)), ItemGroup.BUILDING_BLOCKS);
+	public static final Block POLISHED_BLACKSTONE_SLAB = register("polished_blackstone_vertical_slab", new VerticalSlabBlock(Settings.copy(Blocks.POLISHED_BLACKSTONE_SLAB)), ItemGroup.BUILDING_BLOCKS);
 
 	public static final Block DARK_PRISMARINE_WALL = register("dark_prismarine_wall", new WallBlock(Settings.copy(Blocks.DARK_PRISMARINE)), ItemGroup.DECORATIONS);
 
@@ -964,12 +969,14 @@ public class MubbleBlocks {
 
 	/* CASTLEVANIA */
 	public static final Block VAMPIRE_STONE = register("vampire_stone", new Block(Settings.copy(Blocks.STONE)), ItemGroup.BUILDING_BLOCKS);
+
 	public static final Block MEDUSA_STONE = register("medusa_stone", new Block(Settings.copy(Blocks.STONE)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block MEDUSA_BRICKS = register("medusa_bricks", new Block(Settings.copy(Blocks.STONE_BRICKS)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block MEDUSA_BRICK_STAIRS = register("medusa_brick_stairs", new StairsBlock(MEDUSA_BRICKS), ItemGroup.BUILDING_BLOCKS);
 	public static final Block MEDUSA_BRICK_SLAB = register("medusa_brick_slab", new SlabBlock(Settings.copy(MEDUSA_BRICKS)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block MEDUSA_BRICK_VERTICAL_SLAB = register("medusa_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(MEDUSA_BRICKS)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block MEDUSA_BRICK_WALL = register("medusa_brick_wall", new WallBlock(Settings.copy(MubbleBlocks.MEDUSA_BRICKS)), ItemGroup.DECORATIONS);
+
 	public static final Block WHITE_CANDY_CANE_PILLAR = register("white_candy_cane_pillar", new PillarBlock(FabricBlockSettings.of(Material.STONE, DyeColor.WHITE).hardness(0.2F)), ItemGroup.DECORATIONS, 5, 10);
 	public static final Block LIGHT_GRAY_CANDY_CANE_PILLAR = register("light_gray_candy_cane_pillar", new PillarBlock(FabricBlockSettings.of(Material.STONE, DyeColor.LIGHT_GRAY).hardness(0.2F)), ItemGroup.DECORATIONS, 5, 10);
 	public static final Block GRAY_CANDY_CANE_PILLAR = register("gray_candy_cane_pillar", new PillarBlock(FabricBlockSettings.of(Material.STONE, DyeColor.GRAY).hardness(0.2F)), ItemGroup.DECORATIONS, 5, 10);
@@ -1048,16 +1055,19 @@ public class MubbleBlocks {
 
 	/* CELESTE */
 	public static final Block GIRDER = register("girder", new Block(Settings.copy(Blocks.IRON_BLOCK)), ItemGroup.BUILDING_BLOCKS);
+
 	public static final Block MIRROR_TEMPLE_BRICKS = register("mirror_temple_bricks", new Block(Settings.copy(Blocks.BRICKS)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block MIRROR_TEMPLE_BRICK_STAIRS = register("mirror_temple_brick_stairs", new StairsBlock(MIRROR_TEMPLE_BRICKS), ItemGroup.BUILDING_BLOCKS);
 	public static final Block MIRROR_TEMPLE_BRICK_SLAB = register("mirror_temple_brick_slab", new SlabBlock(Settings.copy(MIRROR_TEMPLE_BRICKS)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block MIRROR_TEMPLE_BRICK_VERTICAL_SLAB = register("mirror_temple_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(MIRROR_TEMPLE_BRICKS)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block MIRROR_TEMPLE_BRICK_WALL = register("mirror_temple_brick_wall", new WallBlock(Settings.copy(MIRROR_TEMPLE_BRICKS)), ItemGroup.DECORATIONS);
+
 	public static final Block OLD_SITE_BRICKS = register("old_site_bricks", new Block(Settings.copy(Blocks.BRICKS)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block OLD_SITE_BRICK_STAIRS = register("old_site_brick_stairs", new StairsBlock(OLD_SITE_BRICKS), ItemGroup.BUILDING_BLOCKS);
 	public static final Block OLD_SITE_BRICK_SLAB = register("old_site_brick_slab", new SlabBlock(Settings.copy(OLD_SITE_BRICKS)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block OLD_SITE_BRICK_VERTICAL_SLAB = register("old_site_brick_vertical_slab", new VerticalSlabBlock(Settings.copy(OLD_SITE_BRICKS)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block OLD_SITE_BRICK_WALL = register("old_site_brick_wall", new WallBlock(Settings.copy(OLD_SITE_BRICKS)), ItemGroup.DECORATIONS);
+
 	public static final Block ELDER_PEBBLES = register("elder_pebbles", new Block(FabricBlockSettings.of(Material.STONE, MaterialColor.RED).strength(2.0F, 6.0F).lightLevel(5)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block DREAM_BLOCK = register("dream_block", new Block(FabricBlockSettings.of(Material.SOLID_ORGANIC).hardness(0.4f).sounds(MubbleSoundTypes.DREAM_BLOCK)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block DREAM_BEDROCK = register("dream_bedrock", new Block(FabricBlockSettings.of(Material.STONE).strength(-1.0F, 3600000.0F).dropsNothing()), ItemGroup.BUILDING_BLOCKS);
