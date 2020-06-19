8cd5b6af5b65becab50ff5d8389ea2638a94ff9e
Hugman <contact.hugman@gmail.com>
2020-06-19 10:18:56 +0200

    Fixed lightsabers

--- src/main/java/hugman/mubble/mixin/PlayerEntityMixin.java
+++ src/main/java/hugman/mubble/mixin/PlayerEntityMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,7 +20,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
-	private static ItemStack prevStack;
+	@Shadow
+	private ItemStack selectedItem;
 
 	@Inject(method = "interact", at = @At(value = "TAIL"), cancellable = true)
 	private void interact(Entity entity, Hand hand, CallbackInfoReturnable<ActionResult> info) {
@@ -38,22 +40,20 @@ public class PlayerEntityMixin {
 		}
 	}
 
-	@Inject(method = "tick", at = @At(value = "TAIL"), cancellable = true)
+	@Inject(method = "tick", at = @At(value = "HEAD"), cancellable = true)
 	private void tick(CallbackInfo info) {
 		PlayerEntity player = (PlayerEntity) (Object) this;
 		World world = player.getEntityWorld();
-		if(prevStack == null) {
-			prevStack = player.getMainHandStack();
-		}
-		ItemStack currentStack = player.getMainHandStack();
-		if(prevStack.getItem() != currentStack.getItem()) {
-			if(currentStack.getItem() instanceof LightsaberItem) {
-				((LightsaberItem) currentStack.getItem()).onPullOut(player, world);
+		ItemStack itemStack = player.getMainHandStack();
+		if(!ItemStack.areEqual(selectedItem, itemStack)) {
+			if(!ItemStack.areItemsEqual(selectedItem, itemStack)) {
+				if(itemStack.getItem() instanceof LightsaberItem) {
+					((LightsaberItem) itemStack.getItem()).onPullOut(player, world);
+				}
+				if(selectedItem.getItem() instanceof LightsaberItem) {
+					((LightsaberItem) selectedItem.getItem()).onPullIn(player, world);
+				}
 			}
-			if(prevStack.getItem() instanceof LightsaberItem) {
-				((LightsaberItem) prevStack.getItem()).onPullIn(player, world);
-			}
 		}
-		prevStack = player.getMainHandStack();
 	}
 }
