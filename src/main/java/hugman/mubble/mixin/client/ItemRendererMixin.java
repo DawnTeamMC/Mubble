64a089aa59daa1633843c3747cf0d34619f614f7
Hugman <contact.hugman@gmail.com>
2020-06-20 00:00:29 +0200

    Tweaks to the Curse of Ignorance

--- src/main/java/hugman/mubble/mixin/client/ItemRendererMixin.java
+++ src/main/java/hugman/mubble/mixin/client/ItemRendererMixin.java
@@ -1,20 +1,29 @@
 package hugman.mubble.mixin.client;
 
+import hugman.mubble.Mubble;
+import hugman.mubble.init.MubbleEnchantments;
+import hugman.mubble.util.EnchantmentUtil;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.List;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ItemRenderer.class)
 public class ItemRendererMixin {
-	@Inject(method = "renderGuiItemOverlay", at = @At(value = "TAIL"), cancellable = true)
-	private void renderGuiItemOverlay(TextRenderer fontRenderer, ItemStack stack, int x, int y, String amountText, CallbackInfoReturnable<List<Text>> info) {
-		//bruh just cancel the durability from showing up
+	private static final String ItemStack = "Lnet/minecraft/item/ItemStack;";
+	private static final String TextRenderer = "Lnet/minecraft/client/font/TextRenderer;";
+	private static final String String = "Ljava/lang/String;";
+
+	@Redirect(method = "renderGuiItemOverlay(" + TextRenderer + ItemStack + "II" + String + ")V", at = @At(value = "INVOKE",target = "Lnet/minecraft/item/ItemStack;isDamaged()Z"))
+	public boolean hideDamage(ItemStack stack) {
+		ClientPlayerEntity clientPlayerEntity = MinecraftClient.getInstance().player;
+		if(EnchantmentUtil.hasEnchantment(MubbleEnchantments.IGNORANCE_CURSE, stack) && !clientPlayerEntity.isCreative()) {
+			return false;
+		}
+		return stack.isDamaged();
 	}
 }
--- src/main/java/hugman/mubble/mixin/client/ItemStackMixin.java
+++ src/main/java/hugman/mubble/mixin/client/ItemStackMixin.java
@@ -3,37 +3,40 @@ package hugman.mubble.mixin.client;
 import com.google.common.collect.Lists;
 import hugman.mubble.init.MubbleEnchantments;
 import hugman.mubble.util.EnchantmentUtil;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-	@Inject(method = "getTooltip", at = @At(value = "TAIL"), cancellable = true)
-	private void getTooltip(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> info) {
-		ItemStack itemStack = (ItemStack) (Object) this;
-		if(player != null) {
-			if(!player.isCreative() & EnchantmentUtil.hasEnchantment(MubbleEnchantments.IGNORANCE_CURSE, itemStack)) {
-				List<Text> tooltip = Lists.newArrayList();
-				MutableText mutableText = (new LiteralText("")).append(itemStack.getName()).formatted(itemStack.getRarity().formatting);
-				if(itemStack.hasCustomName()) {
-					mutableText.formatted(Formatting.ITALIC);
-				}
-				tooltip.add(mutableText);
-				tooltip.add(MubbleEnchantments.IGNORANCE_CURSE.getName(EnchantmentHelper.getLevel(MubbleEnchantments.IGNORANCE_CURSE, itemStack)));
-				info.setReturnValue(tooltip);
-			}
+	@Redirect(method = "getTooltip", at = @At(value = "INVOKE", target = "appendEnchantments"))
+	public void hideEnchantments(List<Text> tooltip, ListTag enchantments, PlayerEntity playerEntity, TooltipContext context) {
+		ItemStack stack = (ItemStack) (Object) this;
+		if(!playerEntity.isCreative() && EnchantmentUtil.hasEnchantment(MubbleEnchantments.IGNORANCE_CURSE, stack)) {
+			tooltip.add(MubbleEnchantments.IGNORANCE_CURSE.getName(EnchantmentHelper.getLevel(MubbleEnchantments.IGNORANCE_CURSE, stack)));
+		}
+		else {
+			stack.appendEnchantments(tooltip, enchantments);
 		}
 	}
 }
