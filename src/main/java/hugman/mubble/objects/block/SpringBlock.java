47755c91d8466def904f1ed60ad6be9bf4eb10ce
Hugman <contact.hugman@gmail.com>
2020-06-30 18:26:06 +0200

    Fixed springs not being able to be placed anywhere

--- src/main/java/hugman/mubble/objects/block/SpringBlock.java
+++ src/main/java/hugman/mubble/objects/block/SpringBlock.java
@@ -2,6 +2,7 @@ package hugman.mubble.objects.block;
 
 import hugman.mubble.init.MubbleSounds;
 import net.minecraft.block.*;
+import net.minecraft.block.enums.WallMountLocation;
 import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.entity.Entity;
 import net.minecraft.fluid.FluidState;
@@ -10,7 +11,6 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.state.StateManager.Builder;
 import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -19,11 +19,8 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
-import net.minecraft.world.WorldView;
 
-public class SpringBlock extends DirectionalBlock implements Waterloggable {
-	public static final DirectionProperty FACING = Properties.FACING;
+public class SpringBlock extends WallMountedBlock implements Waterloggable {
 	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 	private static final VoxelShape IRON_UP = Block.createCuboidShape(6.0D, 0.0D, 6.0D, 10.0D, 6.0D, 10.0D);
 	private static final VoxelShape PLATE_UP = Block.createCuboidShape(1.0D, 6.0D, 1.0D, 15.0D, 9.0D, 15.0D);
@@ -52,7 +49,7 @@ public class SpringBlock extends DirectionalBlock implements Waterloggable {
 
 	public SpringBlock(Block.Settings builder) {
 		super(builder);
-		this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.UP).with(WATERLOGGED, Boolean.valueOf(false)));
+		this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH).with(FACE, WallMountLocation.WALL).with(WATERLOGGED, Boolean.valueOf(false)));
 	}
 
 	@Override
@@ -62,8 +59,7 @@ public class SpringBlock extends DirectionalBlock implements Waterloggable {
 
 	@Override
 	protected void appendProperties(Builder<Block, BlockState> builder) {
-		builder.add(WATERLOGGED);
-		super.appendProperties(builder);
+		builder.add(FACING, FACE, WATERLOGGED);
 	}
 
 	@Override
@@ -73,41 +69,47 @@ public class SpringBlock extends DirectionalBlock implements Waterloggable {
 
 	@Override
 	public VoxelShape getOutlineShape(BlockState state, BlockView worldIn, BlockPos pos, ShapeContext context) {
-		switch(state.get(FACING)) {
-			case UP:
+		switch(state.get(FACE)) {
+			case FLOOR:
 				return SPRING_UP;
-			case DOWN:
-				return SPRING_DOWN;
-			case NORTH:
-				return SPRING_NORTH;
-			case SOUTH:
-				return SPRING_SOUTH;
-			case EAST:
-				return SPRING_EAST;
-			case WEST:
-				return SPRING_WEST;
+			case WALL:
+				switch(state.get(FACING)) {
+					case NORTH:
+					default:
+						return SPRING_NORTH;
+					case SOUTH:
+						return SPRING_SOUTH;
+					case EAST:
+						return SPRING_EAST;
+					case WEST:
+						return SPRING_WEST;
+				}
+			case CEILING:
 			default:
-				return SPRING_UP;
+				return SPRING_DOWN;
 		}
 	}
 
 	@Override
 	public VoxelShape getCollisionShape(BlockState state, BlockView worldIn, BlockPos pos, ShapeContext context) {
-		switch(state.get(FACING)) {
-			case UP:
+		switch(state.get(FACE)) {
+			case FLOOR:
 				return COL_SPRING_UP;
-			case DOWN:
-				return COL_SPRING_DOWN;
-			case NORTH:
-				return COL_SPRING_NORTH;
-			case SOUTH:
-				return COL_SPRING_SOUTH;
-			case EAST:
-				return COL_SPRING_EAST;
-			case WEST:
-				return COL_SPRING_WEST;
+			case WALL:
+				switch(state.get(FACING)) {
+					case NORTH:
+					default:
+						return COL_SPRING_NORTH;
+					case SOUTH:
+						return COL_SPRING_SOUTH;
+					case EAST:
+						return COL_SPRING_EAST;
+					case WEST:
+						return COL_SPRING_WEST;
+				}
+			case CEILING:
 			default:
-				return COL_SPRING_UP;
+				return COL_SPRING_DOWN;
 		}
 	}
 
@@ -117,51 +119,55 @@ public class SpringBlock extends DirectionalBlock implements Waterloggable {
 	}
 
 	@Override
-	public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-		Direction direction = state.get(FACING);
-		return Block.sideCoversSmallSquare(world, pos.offset(direction.getOpposite()), direction);
-	}
-
-	@Override
-	public BlockState getStateForNeighborUpdate(BlockState stateIn, Direction facing, BlockState facingState, WorldAccess worldIn, BlockPos currentPos, BlockPos facingPos) {
-		return facing.getOpposite() == stateIn.get(FACING) && !stateIn.canPlaceAt(worldIn, currentPos) ? Blocks.AIR.getDefaultState() : stateIn;
-	}
-
-	@Override
 	public BlockState getPlacementState(ItemPlacementContext context) {
-		return this.getDefaultState().with(WATERLOGGED, Boolean.valueOf(context.getWorld().getFluidState(context.getBlockPos()).getFluid() == Fluids.WATER)).with(FACING, context.getPlayerFacing());
+		BlockState baseState = super.getPlacementState(context);
+		FluidState fluidState = context.getWorld().getFluidState(context.getBlockPos());
+		if(baseState != null) {
+			return baseState.with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+		}
+		else {
+			return null;
+		}
 	}
 
 	@Override
-	public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+	public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entityIn) {
 		Vec3d vec3d = entityIn.getVelocity();
 		double keptXFactor = vec3d.x / 3;
 		double keptYFactor = vec3d.y / 3;
 		double keptZFactor = vec3d.z / 3;
-		switch(state.get(FACING)) {
-			case UP:
+		switch(state.get(FACE)) {
+			case FLOOR:
 				entityIn.setVelocity(vec3d.x, keptYFactor + 1.5D, vec3d.z);
 				break;
-			case DOWN:
-				entityIn.setVelocity(vec3d.x, keptYFactor + -1.5D, vec3d.z);
+			case WALL:
+				switch(state.get(FACING)) {
+					case NORTH:
+					default:
+						entityIn.setVelocity(vec3d.x, keptYFactor + 0.2D, keptZFactor + -1.5D);
+						break;
+					case SOUTH:
+						entityIn.setVelocity(vec3d.x, keptYFactor + 0.2D, keptZFactor + 1.5D);
+						break;
+					case EAST:
+						entityIn.setVelocity(keptXFactor + 1.5D, keptYFactor + 0.2D, vec3d.z);
+						break;
+					case WEST:
+						entityIn.setVelocity(keptXFactor + -1.5D, keptYFactor + 0.2D, vec3d.z);
+						break;
+				}
 				break;
-			case NORTH:
-				entityIn.setVelocity(vec3d.x, keptYFactor + 0.2D, keptZFactor + -1.5D);
-				break;
-			case SOUTH:
-				entityIn.setVelocity(vec3d.x, keptYFactor + 0.2D, keptZFactor + 1.5D);
-				break;
-			case EAST:
-				entityIn.setVelocity(keptXFactor + 1.5D, keptYFactor + 0.2D, vec3d.z);
-				break;
-			case WEST:
-				entityIn.setVelocity(keptXFactor + -1.5D, keptYFactor + 0.2D, vec3d.z);
-				break;
+			case CEILING:
 			default:
+				entityIn.setVelocity(vec3d.x, keptYFactor + -1.5D, vec3d.z);
 				break;
 		}
 		entityIn.fallDistance = 0f;
-		worldIn.playSound(null, pos, MubbleSounds.BLOCK_SPRING_TRIGGER, SoundCategory.BLOCKS, 1f, 1f);
+		if (!world.isClient)
+		{
+
+			world.playSound(null, pos, MubbleSounds.BLOCK_SPRING_TRIGGER, SoundCategory.BLOCKS, 0.6f, 1f);
+		}
 	}
 
 	@Override
