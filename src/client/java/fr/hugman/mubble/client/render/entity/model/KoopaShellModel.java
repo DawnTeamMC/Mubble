652291c7ce6e79516235f9391ffecb2b692bdafe
Hugman <contact.hugman@gmail.com>
2024-09-30 18:29:46 +0200

    Complete the update for 24w39a

--- src/client/java/fr/hugman/mubble/client/render/entity/model/KoopaShellModel.java
+++ src/client/java/fr/hugman/mubble/client/render/entity/model/KoopaShellModel.java
@@ -1,28 +1,13 @@
 package fr.hugman.mubble.client.render.entity.model;
 
-import com.google.common.collect.ImmutableList;
-import fr.hugman.mubble.entity.KoopaShellEntity;
+import fr.hugman.mubble.client.render.entity.state.KoopaShellEntityRenderState;
 import net.minecraft.client.model.*;
-import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.entity.model.EntityModelPartNames;
-import net.minecraft.client.util.math.MatrixStack;
 
-public class KoopaShellModel extends EntityModel<KoopaShellEntity> {
-    private final ModelPart base;
-
-    public KoopaShellModel(ModelPart modelPart) {
-        this.base = modelPart.getChild(EntityModelPartNames.CUBE);
-    }
-
-    @Override
-    public void setAngles(KoopaShellEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
-
-    }
-
-    @Override
-    public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int color) {
-        ImmutableList.of(this.base).forEach((modelRenderer) -> modelRenderer.render(matrices, vertices, light, overlay, color));
+public class KoopaShellModel extends EntityModel<KoopaShellEntityRenderState> {
+    public KoopaShellModel(ModelPart part) {
+        super(part.getChild(EntityModelPartNames.CUBE));
     }
 
     public static TexturedModelData getTexturedModelData() {
---
+++ src/client/java/fr/hugman/mubble/client/render/entity/state/KoopaShellEntityRenderState.java
@@ -0,0 +1,16 @@
+package fr.hugman.mubble.client.render.entity.state;
+
+import fr.hugman.mubble.Mubble;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.entity.state.EntityRenderState;
+import net.minecraft.util.Identifier;
+
+@Environment(EnvType.CLIENT)
+public class KoopaShellEntityRenderState extends EntityRenderState {
+    private static final Identifier DEFAULT_TEXTURE = Mubble.id("textures/entity/koopa_shell/green.png");
+
+    public Identifier texture = DEFAULT_TEXTURE;
+    public boolean invisibleToPlayer;
+    public boolean hasOutline;
+}
