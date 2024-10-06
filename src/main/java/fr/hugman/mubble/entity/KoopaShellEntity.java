524f8703e69aab234c15f314d704621804321235
Hugman <contact.hugman@gmail.com>
2024-10-06 15:56:58 +0200

    Make Koopa shell collision detection square

--- src/main/java/fr/hugman/mubble/entity/KoopaShellEntity.java
+++ src/main/java/fr/hugman/mubble/entity/KoopaShellEntity.java
@@ -1,7 +1,6 @@
 package fr.hugman.mubble.entity;
 
-import fr.hugman.mubble.Mubble;
-import net.minecraft.client.MinecraftClient;
+import fr.hugman.mubble.util.BoxUtil;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MovementType;
@@ -12,10 +11,12 @@ import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class KoopaShellEntity extends ProjectileEntity {
     public KoopaShellEntity(EntityType<? extends KoopaShellEntity> entityType, World world) {
@@ -49,10 +50,13 @@ public class KoopaShellEntity extends ProjectileEntity {
         this.hitOrDeflect(hitResult);
 
         this.applyGravity();
-        var multiplier = this.calculateBouncingMultiplier();
+
+        Box hitBox = this.getBoundingBox().offset(this.getVelocity().x > 0 ? 0.01d : -0.01d, 0.0d, this.getVelocity().z > 0 ? 0.01d : -0.01d);
+
+        var multiplier = BoxUtil.calculateBouncingMultiplier(hitBox, BoxUtil.collectPotentialBlockCollisions(this.getWorld(), hitBox));
         var prevVelocity = this.getVelocity();
         this.move(MovementType.SELF, prevVelocity);
-        if(multiplier != null) {
+        if (multiplier != null) {
             prevVelocity = prevVelocity.multiply(multiplier);
             // TODO: PLAY SOUND
             // TODO: PLAY PARTICLES
@@ -71,48 +75,5 @@ public class KoopaShellEntity extends ProjectileEntity {
         super.onEntityHit(result);
         result.getEntity().serverDamage(this.getDamageSources().thrown(this, this.getOwner()), 2.0F);
         // TODO: PLAY SOUND
-    }
-
-    @Nullable
-    public Vec3d calculateBouncingMultiplier() {
-        var shellBox = this.getBoundingBox().offset(this.getVelocity().x > 0 ? 0.01d : -0.01d, 0.0d, this.getVelocity().z > 0 ? 0.01d : -0.01d).shrink(0.0d, 0.01d, 0.0d);
-        var shellCenter = shellBox.getCenter();
-        var radius = (Math.sqrt(2) / 2.0d) * this.getWidth();
-
-        Iterable<BlockPos> iterable = BlockPos.iterate(shellBox);
-
-        double minDistanceX = Double.MAX_VALUE;
-        double minDistanceZ = Double.MAX_VALUE;
-
-        for (BlockPos pos : iterable) {
-            var boundingBoxes = this.getWorld().getBlockState(pos).getCollisionShape(this.getWorld(), pos).offset(Vec3d.of(pos)).getBoundingBoxes();
-            for(Box box : boundingBoxes) {
-                double closestX = MathHelper.clamp(shellCenter.getX(), box.minX, box.maxX);
-                double closestZ = MathHelper.clamp(shellCenter.getZ(), box.minZ, box.maxZ);
-                double distanceX = shellCenter.getX() - closestX;
-                double distanceZ = shellCenter.getZ() - closestZ;
-                if(Math.sqrt(MathHelper.square(distanceX) + MathHelper.square(distanceZ)) > radius) {
-                    continue;
-                }
-                if(Math.abs(distanceX) < Math.abs(minDistanceX)) {
-                    minDistanceX = Math.abs(distanceX);
-                }
-                if(Math.abs(distanceZ) < Math.abs(minDistanceZ)) {
-                    minDistanceZ = Math.abs(distanceZ);
-                }
-            }
-        }
-        if(minDistanceX == Double.MAX_VALUE && minDistanceZ == Double.MAX_VALUE) {
-            return null;
-        }
-
-        if (minDistanceX == minDistanceZ) {
-            return new Vec3d(-1.0, 1.0D, -1.0D);
-        }
-        if (minDistanceX > minDistanceZ) {
-            return new Vec3d(-1.0, 1.0D, 1.0D);
-        } else {
-            return new Vec3d(1.0, 1.0D, -1.0D);
-        }
     }
 }
---
+++ src/main/java/fr/hugman/mubble/util/BoxUtil.java
@@ -0,0 +1,89 @@
+package fr.hugman.mubble.util;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility class for box collision calculations.
+ *
+ * @author Hugman
+ * @since v4.0.0
+ */
+public class BoxUtil {
+    /**
+     * Calculates the bouncing multiplier vector based on the collision of
+     * the given origin box with a list of other boxes.
+     * <p>
+     * This method checks for collisions in both the X and Z axes. If a
+     * collision is detected, it computes the minimum distance to the
+     * closest edge of the colliding boxes in both axes. The method returns
+     * a vector indicating the bounce direction. If there is no collision,
+     * it returns null.
+     *
+     * @param originBox the box for which the bouncing multiplier is calculated
+     * @param otherBoxes a list of other boxes to check for collisions against
+     * @return a {@link Vec3d} representing the bouncing multiplier direction,
+     *         or null if no collision is detected
+     */
+    @Nullable
+    public static Vec3d calculateBouncingMultiplier(Box originBox, List<Box> otherBoxes) {
+        double minDistanceX = Double.MAX_VALUE;
+        double minDistanceZ = Double.MAX_VALUE;
+
+        for (Box box : otherBoxes) {
+            // Check for collision between the origin box and the other box
+            if (originBox.maxX > box.minX && originBox.minX < box.maxX &&
+                originBox.maxZ > box.minZ && originBox.minZ < box.maxZ) {
+
+                // Calculate the distances to the closest edges of the box
+                double distanceX = Math.min(originBox.maxX - box.minX, box.maxX - originBox.minX);
+                double distanceZ = Math.min(originBox.maxZ - box.minZ, box.maxZ - originBox.minZ);
+
+                // Update the minimum distances for collision response
+                if (Math.abs(distanceX) < Math.abs(minDistanceX)) {
+                    minDistanceX = distanceX;
+                }
+                if (Math.abs(distanceZ) < Math.abs(minDistanceZ)) {
+                    minDistanceZ = distanceZ;
+                }
+            }
+        }
+
+        // Check if a collision was detected
+        if (minDistanceX == Double.MAX_VALUE && minDistanceZ == Double.MAX_VALUE) {
+            return null;  // No collision detected
+        }
+
+        // Determine which axis the collision is on
+        return (minDistanceX < minDistanceZ)
+                ? new Vec3d(-1.0, 1.0, 1.0)  // Bounce in the x-axis
+                : new Vec3d(1.0, 1.0, -1.0);  // Bounce in the z-axis
+    }
+
+    /**
+     * Collects potential block collisions in the world for the given origin box.
+     * <p>
+     * This method iterates over all block positions within the bounds of
+     * the origin box and retrieves the collision shapes for each block
+     * position.
+     *
+     * @param originBox the box to check for potential collisions
+     * @return a list of {@link Box} objects representing the potential collisions
+     */
+    public static List<Box> collectPotentialBlockCollisions(World world, Box originBox) {
+        Iterable<BlockPos> iterable = BlockPos.iterate(originBox);
+        List<Box> boundingBoxes = new ArrayList<>();
+        for (BlockPos pos : iterable) {
+            // Collect bounding boxes from collision shapes directly
+            boundingBoxes.addAll(world.getBlockState(pos).getCollisionShape(world, pos)
+                    .offset(Vec3d.of(pos)).getBoundingBoxes());
+        }
+        return boundingBoxes;
+    }
+}
