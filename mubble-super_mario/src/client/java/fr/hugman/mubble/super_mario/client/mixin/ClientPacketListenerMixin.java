a040fddd119ad1dc3fa39709e48e6786397108ff
Hugman <contact.hugman@gmail.com>
2026-02-12 22:57:36 +0100

    Remove useless accessor mixin

--- mubble-super_mario/src/client/java/fr/hugman/mubble/super_mario/client/mixin/ClientPacketListenerMixin.java
+++ mubble-super_mario/src/client/java/fr/hugman/mubble/super_mario/client/mixin/ClientPacketListenerMixin.java
@@ -6,6 +6,7 @@ import fr.hugman.mubble.super_mario.world.entity.projectile.KoopaShell;
 import fr.hugman.mubble.super_mario.world.entity.projectile.RedKoopaShell;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,14 +17,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPacketListener.class)
 @Environment(EnvType.CLIENT)
 public class ClientPacketListenerMixin {
-    @Inject(method = "postAddEntitySoundInstance", at = @At("HEAD"))
-    private void onPlaySpawnSound(Entity entity, CallbackInfo ci) {
-        ClientCommonPacketListenerImplAccessor accessor = (ClientCommonPacketListenerImplAccessor) this;
+    @Inject(method = "postAddEntitySoundInstance", at = @At("TAIL"))
+    private void super_mario$postAddEntitySoundInstance(Entity entity, CallbackInfo ci) {
         if (entity instanceof RedKoopaShell koopaShell) {
-            accessor.getClient().getSoundManager().play(new HomingKoopaShellSoundInstance(koopaShell));
+            Minecraft.getInstance().getSoundManager().play(new HomingKoopaShellSoundInstance(koopaShell));
         }
         else if (entity instanceof KoopaShell koopaShell) {
-            accessor.getClient().getSoundManager().play(new SlidingKoopaShellSoundInstance(koopaShell));
+            Minecraft.getInstance().getSoundManager().play(new SlidingKoopaShellSoundInstance(koopaShell));
         }
     }
 }
